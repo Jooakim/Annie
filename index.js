42a00bb02d1ec49fc64a8e361aed7d9f3e23ba44
Phuc-T-Tran <phuc978@gmail.com>
2017-01-28 16:49:16 -0500

    Testing different message responses.

--- index.js
+++ index.js
@@ -21,16 +21,65 @@ app.get('/webhook', function (req, res) {
     }
 });
 
+// Handle user messages
 app.post('/webhook', function (req, res) {  
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
-        var event = events[i];
+        let event = events[i];
+        let sender = event.sender.id;
+
+        // Check if a message and text string exist
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            let message = "";
+            switch(event.message.text) {
+                case "menu":
+                    sendMessage(sender, {text: "--DISPLAY MENU--"});
+                    break;
+                case "test":
+                    sendMessage(sender, {text: "--SENDING TEST MESSAGE--"});
+                    sendTestMessage(sender);
+                    break;
+                default:
+                   sendMessage(sender, {text: "Echo: " + event.message.text});
+                    break;
+            }
+            
         }
     }
     res.sendStatus(200);
 });
+
+// Testing
+function sendTestMessage(recipientId) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "Add Prescriptions",
+            buttons: [{
+              type: "postback",
+              title: "Call Postback 1",
+              payload: "Postback 1 Called",
+            }, {
+              type: "postback",
+              title: "Call Postback 2",
+              payload: "Postback 2 Called",
+            }],
+          }]
+        }
+      }
+    }
+  };  
+
+  sendMessage(recipientId, messageData);
+}
+
 
 // generic function sending messages
 function sendMessage(recipientId, message) {  
