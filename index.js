f5aa589799f9d8f81d76294889c31b4845cb3887
Simon Larsson Takman <simontakman@gmail.com>
2017-01-28 14:30:23 -0500

    Added functionality to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-s = require('express');  
+var express = require('express');  
 var bodyParser = require('body-parser');  
 var request = require('request');  
 var app = express();
@@ -14,9 +14,20 @@ app.get('/', function (req, res) {
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {  
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+    if (req.query['hub.verify_token'] === 'OttawaMcHacks17') {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Invalid verify token');
     }
+});
+
+app.post('/webhook', function (req, res) {  
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
+    }
+    res.sendStatus(200);
 });
