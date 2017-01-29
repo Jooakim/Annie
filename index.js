5db968b607d322242ea2ffa79a35c890f70fc615
joakim <joakim.0565@gmail.com>
2017-01-29 01:08:11 -0500

    Remove payload

--- index.js
+++ index.js
@@ -89,15 +89,6 @@ app.post('/webhook', function (req, res) {
                     sendMessage(sender, {text: "Echo: " + event.message.text});
                     break;
             }
-        } else if (event.payload) {
-            switch(event.payload) {
-                case "PAYLOAD_ADD":
-                    showAddMenu(sender);
-                    break;
-                case "PAYLOAD_REMOVE":
-                    showRemoveMenu(sender);
-                    break;
-            }
         }
     }
     res.sendStatus(200);
