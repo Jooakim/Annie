cf73f6700df0c954d97ed7002ad238dc9fc73f00
joakim <joakim.0565@gmail.com>
2017-01-28 19:20:13 -0500

    Revert "Extract time information"
    This reverts commit 5cf28938c244c8ad12f573c5b119ffe4dfcc7b16.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var express = require('express');  
+var bodyParser = require('body-parser');  
 var request = require('request');  
 var annie = require('./annie.js')
 var app = express();
@@ -40,7 +42,7 @@ app.post('/webhook', function (req, res) {
                     sendMessage(sender, {text: "Commands:\n !add, !remove, !status, !ice (In Case of Emergency)"});
                     break;
                 case "medsTest":
-                    sendMessage(sender, {text: annie.getMedications(0) + " " + req.body.entry[0].time });
+                    sendMessage(sender, {text: annie.getMedications(0)});
                     break;
                 case "simon":
                     var output = annie.getDummyJson(0);
