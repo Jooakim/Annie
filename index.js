bee92d5bef9660d0eb316367fd0ba3669e82bc24
Simon Larsson Takman <simontakman@gmail.com>
2017-01-28 20:56:42 -0500

    Added pg.sql

--- index.js
+++ index.js
@@ -196,18 +196,14 @@ function kittenMessage(recipientId, text) {
 /*-------------------------------------------------------------------------------------------------------------------- */
 /*-------------------------------------------------------------------------------------------------------------------- */
 
-/*
 pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
+if (err) console.log('Cant reach database');
   console.log('Connected to postgres! Getting schemas...');
-
-  client
+  /*client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
-    });
+    });*/
 });
-
-*/
 
